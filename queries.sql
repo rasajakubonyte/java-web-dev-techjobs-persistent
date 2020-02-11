607ca41e03498e36213209b90e90c4d71d907be7
Rasa Jakubonyte <rasa@Nathans-MacBook-Pro.local>
2020-02-11 17:36:45 -0600

    SQL Added

--- queries.sql
+++ queries.sql
@@ -1,16 +1,23 @@
 ## Part 1: Test it with SQL
+CREATE TABLE job(
+     id INT,
+    name VARCHAR(25),
+    employer VARCHAR(75),
+    skills VARCHAR(25)
+    );
 
-
-    private int id;
-
-    private String name;
-
-    private String employer;
-    private String skills;
 
 ## Part 2: Test it with SQL
-
+SELECT employer
+FROM job
+WHERE employer = "St. Louis";
 
 ## Part 3: Test it with SQL
+DROP TABLE job;
+
 
 ## Part 4: Test it with SQL
+SELECT name, skills
+FROM job
+WHERE name IS NOT NULL
+ORDER BY name, skill ASC;
